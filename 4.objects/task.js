9e68a461b378b8c465b4363ef95abc8dccc1a6fd
Dima Neto <kuznetsovdv2013@gmail.com>
2025-01-25 09:51:39 +0300

    Четвертое задание доработано

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,33 +5,28 @@ function Student(name, gender, age) {
     this.marks = [];
 }
 
-let student = new Student;
 
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;  
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if (student.hasOwnProperty("marks") === true) {
-        this.marks = this.marks.concat(...marks);
+    if (this.hasOwnProperty("marks") === true) {
+        this.marks.push(...marks);
     }
 }
 
 Student.prototype.getAverage = function () {
-
-    let summ = this.marks.reduce((acc, ind) => acc + ind, 0);
-    let average = summ / this.marks.length;
-    if (student.hasOwnProperty("marks") === false && this.marks.length == 0) {
+    if (this.hasOwnProperty("marks") === false || this.marks.length == 0) {
         return 0;
     } 
-    if (student.hasOwnProperty("marks") === true) {
-        return average;
-    }
-
+    let summ = this.marks.reduce((acc, ind) => acc + ind, 0);
+    let average = summ / this.marks.length;
+    return average;
 }
 
 Student.prototype.exclude = function (reason) {
-    delete student.subject;
-    delete student.marks;
-    this.Student.excluded = "reason";
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
